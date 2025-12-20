dcc3520b0b770f8921fcbac8f63fb3d98c40d125
Ritik-developer2006 <rk5771829@gmail.com>
2025-12-21 00:42:45 +0530

    update resume.php page

--- admin/resume.php
+++ admin/resume.php
@@ -15,53 +15,18 @@ require_once("header.php");
             <div class="panel mb-25">
                 <div class="panel-header d-flex justify-content-between">
                     <div><h5 class="fw-bold">Educations</h5></div>
-                    <div><i class="fa-solid fa-circle-plus"></i></div>
+                    <div><i class="fa-solid fa-circle-plus" id="add_education" style="cursor: pointer"></i></div>
                 </div>
-                <div class="panel-body">
-                    <div class="card">
-                        <div class="card-header d-flex justify-content-between">
-                            <div>Diploma</div>
-                            <div><i class="fa-solid fa-pen-to-square"></i></div>
+                <div class="panel-body d-flex flex-column justify-content-center align-items-center" id="education_container">
+                    <!-- data come through ajax -->
+                     <div class="d-flex flex-column justify-content-center align-items-center">
+                        <div>
+                            <i class="fa-solid fa-circle-info fs-1"></i>
                         </div>
-                        <div class="card-body">
-                            <div class="row g-3">
-                                <div class="col-sm-6">
-                                    <label for="inputWithIcon" class="form-label">Input with icon</label>
-                                    <div class="input-group-with-icon">
-                                        <span class="input-icon">
-                                            <i class="fa-light fa-envelope"></i>
-                                        </span>
-                                        <input type="email" class="form-control ps-0" id="inputWithIcon" placeholder="example@info.com">
-                                    </div>
-                                </div>
-
-                                <div class="col-sm-6">
-                                    <label for="inputWithIcon" class="form-label">Input with icon</label>
-                                    <div class="input-group-with-icon">
-                                        <span class="input-icon">
-                                            <i class="fa-light fa-envelope"></i>
-                                        </span>
-                                        <input type="email" class="form-control ps-0" id="inputWithIcon" placeholder="example@info.com">
-                                    </div>
-                                </div>
-                                
-                                <div class="col-sm-6">
-                                    <label for="inputDate" class="form-label">Input date</label>
-                                    <input type="date" class="form-control" id="inputDate">
-                                </div>
-
-                                <div class="col-sm-6">
-                                    <label for="inputDate" class="form-label">Input date</label>
-                                    <input type="date" class="form-control" id="inputDate">
-                                </div>
-                                
-                                <div class="col-sm-12">
-                                    <label for="exampleTextarea" class="form-label">Example textarea</label>
-                                    <textarea class="form-control" id="exampleTextarea"></textarea>
-                                </div>
-                            </div>
+                        <div>
+                            <h5>Data Not Available!</h5>
                         </div>
-                    </div>
+                     </div>
                 </div>
             </div>
         </div>
@@ -190,4 +155,93 @@ require_once("footer.php");
   rangeInput.addEventListener('input', function() {
     rangeOutput.textContent = this.value;
   });
+
+  getAlleducation();
+  function getAlleducation() {
+        $.ajax({
+            type: 'POST',
+            url: '/Ritik Portfolio/API/adminApi.php',
+            dataType: 'JSON',
+            data: JSON.stringify({
+                method: 'getAlleducation'
+            }),
+            contentType: "application/json",
+            beforeSend: function () {
+                $(".preloader").show();
+            },
+            complete: function () {
+                $(".preloader").hide();
+            },
+            success: function (response) {
+                // console.log(response);
+                let status = response.status;
+                if (status == 1) {
+                    let html = "";
+                    if(response.data.length > 0){
+                        $.each(response.data, function (index, item){
+                            html += `<div class="card ${item.id != 1 ? 'mt-3' : ''}">
+                                        <div class="card-header d-flex justify-content-between">
+                                            <div>${item.education_name}</div>
+                                            <div>
+                                            <i class="fa-solid fa-pen-to-square edit_education" style="cursor: pointer;"></i>
+                                            <i class="fa-solid fa-trash delete_education" style="cursor: pointer;"></i>
+                                            </div>
+                                            <input type="hidden" value="${item.id}" class="edu_id">
+                                        </div>
+                                        <div class="card-body">
+                                            <form method="post" action="#">
+                                                <div class="row g-3">
+                                                    <div class="col-sm-6">
+                                                        <label for="university${item.id}" class="form-label">Input with icon</label>
+                                                        <div class="input-group-with-icon">
+                                                            <span class="input-icon">
+                                                                <i class="fa-light fa-envelope"></i>
+                                                            </span>
+                                                            <input type="text" readonly name='university' value="${item.university}" class="form-control ps-0" id="university${item.id}" placeholder="Your university">
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="col-sm-6">
+                                                        <label for="college${item.id}" class="form-label">Input with icon</label>
+                                                        <div class="input-group-with-icon">
+                                                            <span class="input-icon">
+                                                                <i class="fa-light fa-envelope"></i>
+                                                            </span>
+                                                            <input type="text" readonly name='college' value="${item.college_name}" class="form-control ps-0" id="college${item.id}" placeholder="Your college">
+                                                        </div>
+                                                    </div>
+                                                    
+                                                    <div class="col-sm-6">
+                                                        <label for="from_year${item.id}" class="form-label">Input date</label>
+                                                        <input type="date" readonly name='from_year' value="${item.from_year}" class="form-control" id="from_year${item.id}">
+                                                    </div>
+
+                                                    <div class="col-sm-6">
+                                                        <label for="to_year${item.id}" class="form-label">Input date</label>
+                                                        <input type="date" readonly name='to_year' value="${item.to_year}" class="form-control" id="to_year${item.id}">
+                                                    </div>
+                                                    
+                                                    <div class="col-sm-12">
+                                                        <label for="description${item.id}" class="form-label">Example textarea</label>
+                                                        <textarea class="form-control" readonly name="description" id="description${item.id}">${item.description}</textarea>
+                                                    </div>
+
+                                                    <div class="col-sm-12 d-none">
+                                                        <button class="btn btn-sm btn-primary" type="submit">Update</button>
+                                                    </div>
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </div>`;
+                        });
+                        $("#education_container").html(html);
+                    }
+                }
+            },
+            error: function () {
+                console.log("Something went wrong, while geting data of educations!");
+            }
+        });
+    };
+
 </script>
